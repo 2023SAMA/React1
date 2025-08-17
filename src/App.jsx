f67e4a11ff9960a6c83339f9908a8beb6fcaa9d8
2023SAMA <smasyd237@gmail.com>
2025-08-17 15:34:58 -0700

    fix images with BASE_URL

--- src/App.jsx
+++ src/App.jsx
@@ -3,42 +3,42 @@ import Card from './Card';
 
 const items = [
   {
-    img: "pexels-daan-stevens-66128-939331.jpg",
+    img: `${import.meta.env.BASE_URL}pexels-daan-stevens-66128-939331.jpg`,
     name: "Sama",
     price: 9000,
     title: "MacBook Air 13 – M1 Chip",
     decs: "A sleek and lightweight laptop featuring a 14-inch Full HD display, 11th Gen Intel Core i7 processor, 16GB RAM, and 512GB SSD storage. Offers up to 15 hours of battery life and a premium metallic design, perfect for work and study.",
   },
   {
-    img: "pexels-energepic-com-27411-159886.jpg",
+    img: `${import.meta.env.BASE_URL}pexels-energepic-com-27411-159886.jpg`,
     name: "Sama",
     price: 3000,
     title: "HP Spectre x360 – 13.5",
     decs: "A sleek and lightweight laptop featuring a 14-inch Full HD display, 11th Gen Intel Core i7 processor, 16GB RAM, and 512GB SSD storage. Offers up to 15 hours of battery life and a premium metallic design, perfect for work and study.",
   },
   {
-    img: "pexels-hasanalbari-1229861.jpg",
+    img: `${import.meta.env.BASE_URL}pexels-hasanalbari-1229861.jpg`,
     name: "Sama",
     price: 1000,
     title: "Lenovo ThinkPad X1 Carbon Gen 11 ",
     decs: "A sleek and lightweight laptop featuring a 14-inch Full HD display, 11th Gen Intel Core i7 processor, 16GB RAM, and 512GB SSD storage. Offers up to 15 hours of battery life and a premium metallic design, perfect for work and study.",
   },
   {
-    img: "pexels-morningtrain-18105.jpg",
+    img: `${import.meta.env.BASE_URL}pexels-morningtrain-18105.jpg`,
     name: "Sama",
     price: 7000,
     title: "MacBook Pro 16– M2 Pro ",
     decs: "A sleek and lightweight laptop featuring a 14-inch Full HD display, 11th Gen Intel Core i7 processor, 16GB RAM, and 512GB SSD storage. Offers up to 15 hours of battery life and a premium metallic design, perfect for work and study.",
   },
   {
-    img: "pexels-picjumbo-com-55570-196655.jpg",
+    img: `${import.meta.env.BASE_URL}pexels-picjumbo-com-55570-196655.jpg`,
     name: "Sama",
     price: 10000,
     title: "Samsung Galaxy Book3 Pro",
     decs: "A sleek and lightweight laptop featuring a 14-inch Full HD display, 11th Gen Intel Core i7 processor, 16GB RAM, and 512GB SSD storage. Offers up to 15 hours of battery life and a premium metallic design, perfect for work and study.",
   },
   {
-    img: "pexels-pixabay-459653.jpg",
+    img: `${import.meta.env.BASE_URL}pexels-pixabay-459653.jpg`,
     name: "Sama",
     price: 3600,
     title: "ASUS ROG Strix G16 ",
